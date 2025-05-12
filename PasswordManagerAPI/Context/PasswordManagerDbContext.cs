d8f8126d368428d639f32f9579bce7ffe4c2311e
madaljeh <ahmadmadaljeh@gmail.com>
2025-05-13 00:36:02 +0300

    Add project files.

---
+++ PasswordManagerAPI/Context/PasswordManagerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PasswordManagerAPI.Context
+{
+
+
+    public class PasswordManagerDbContext : DbContext
+    {
+        public PasswordManagerDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
